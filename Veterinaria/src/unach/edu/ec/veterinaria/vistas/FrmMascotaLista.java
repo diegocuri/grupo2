b444aa4dbc07f91f08ba29c657d6a0151b9627b1
mishell <SONY@DESKTOP-EIOR6CT>
2018-01-30 17:09:31 -0500

    formulario mascota

---
+++ Veterinaria/src/unach/edu/ec/veterinaria/vistas/FrmMascotaLista.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unach.edu.ec.veterinaria.vistas;
+
+/**
+ *
+ * @author SONY
+ */
+class FrmMascotaLista {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setTitle(String mascotas_registrados) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
