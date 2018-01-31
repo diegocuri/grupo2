55af6bfb843bd08628d711db122443d5fe92aba5
FabricioConstante <Usuario@FI-LABC01-PC01>
2018-01-31 16:26:07 -0500

    entidad etiquetas

--- Veterinaria/src/unach/edu/ec/veterinaria/vistas/FrmMascotaLista.java
+++ Veterinaria/src/unach/edu/ec/veterinaria/vistas/FrmMascotaLista.java
@@ -1,22 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package unach.edu.ec.veterinaria.vistas;
 
+import unach.edu.ec.veterinaria.Entidades.*;
+import unach.edu.ec.veterinaria.dao.*;
+import unach.edu.ec.veterinaria.impl.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 /**
  *
- * @author SONY
- */
-class FrmMascotaLista {
-
-    void setVisible(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void setTitle(String mascotas_registrados) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+ * @author FABRICIO CONSTANTE
+ */ 
+public class FrmMascotaLista extends JInternalFrame{
     
+    JLabel lblTitulo;
+    JTable tabla;
+    DefaultTableModel modelo;
+    public FrmMascotaLista(){
+        this.setSize(800, 600);
+        this.setLayout(new BorderLayout());
+        this.setClosable(true);
+        this.setBackground(Color.getHSBColor(100,60,270).brighter());
+        lblTitulo=new JLabel("Lista de Mascotas");
+        tabla= new JTable();
+        this.add(lblTitulo,BorderLayout.NORTH);
+        this.add(tabla,BorderLayout.NORTH);
+        cargarTabla();        
+    }
+    public void cargarTabla(){
+        modelo = new DefaultTableModel();
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Sexo");
+        modelo.addColumn("Raza");
+        
+        List<Mascota> list = new ArrayList<>();
+        try {
+            IMascota mascotaDao= new MascotaImpl();
+            list = mascotaDao.obtener();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+        }
+        for(Mascota masc : list){
+            modelo.addRow(new Object[]{masc.getCodigo(),masc.getNombre(),masc.getSexo(),masc.getRaza().toString()});
+        }
+        tabla.setModel(modelo);
+    }
 }
